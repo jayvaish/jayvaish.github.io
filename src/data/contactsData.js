3f0d149de26641e8bb640e524c59ba60b509afbc
ajay <vajaysingh2002@gmail.com>
2022-10-19 20:34:46 +0530

    aj port

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'vajaysingh2002@gmail.com',
+    phone: '+91-9753790806',
+    address: 'Burhanpur MadhyaPradesh ',
+
+    sheetAPI: ''
+}
---
+++ src/data/experienceData.js
@@ -0,0 +1,16 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Dhoot Transmisson Private Limited',
+        jobtitle: 'Intrenship',
+        startYear: '1 MAY-2019 ',
+        endYear: '7 June-2020'
+    },
+    {
+        id: 2,
+        company: 'Dhoot Transmisson Private Limited',
+        jobtitle: 'Quality Engineer',
+        startYear: '8 AUG-2020',
+        endYear: '5 June-2021'
+    },
+]
