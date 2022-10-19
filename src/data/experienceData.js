d5c5e8bdcddeb5ab232f5c0e94504979550d1615
ajay <vajaysingh2002@gmail.com>
2022-10-19 13:21:06 +0530

    op

---
+++ src/data/experienceData.js
@@ -0,0 +1,16 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Atul Ltd',
+        jobtitle: 'Intrenship',
+        startYear: '1 MAY-2019 ',
+        endYear: '1 June-2019'
+    },
+    {
+        id: 2,
+        company: 'BosLeo Technology Private Limited',
+        jobtitle: 'Intrenship',
+        startYear: '8 JAN-2020',
+        endYear: '8  June-2020'
+    },
+]
